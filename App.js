a347fdd40d9dff20468c72da3a1cd0dfc951f58f
Hafiz Muhammad Danial <danialijaz098@gmail.com>
2024-11-30 12:59:30 +0500

    Button_Navigaton
    move one screen to another and vice versa

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
+const HomeScreen = ({navigation}) => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Home screen </Text>
+      <Button title="Go to Detail" onPress={() => navigation.navigate('Detail')} />
     </View>
   );
-}
+};
+const DetailScreen = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <Text>Detail Screen</Text>
+      <Button title="Go Back " onPress={() => navigation.goBack()} />
+    </View>
+  );
+};
+
+const Stack = createStackNavigator();
+
+const App = () => (
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Detail" component={DetailScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
